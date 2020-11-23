c35aa5aff9bcf500582682a51fae3b0ce159b695
Demien Blogan <demienblogan@gmail.com>
2020-11-23 14:21:07 +0200

    Add files via upload

---
+++ Examples/Chapter 02/example 2-27.cpp
@@ -0,0 +1,14 @@
+/*
+	PROGRAM: PAYROLL.CPP
+	Written by Herbert Dorfmann
+	This program calculates company payroll
+	Last modification: 8/20/2017
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double payRate;   // Holds the hourly pay rate
+	double hours;     // Hold the hours worked
+	int employNumber; // Holds the employee number
